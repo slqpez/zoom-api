5ca923717beae990b526f01b6dd2d267772fc290
slqpez <santiago.lopez16@udea.edu.co>
2020-09-05 14:44:55 -0500

    Pug.

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const home = require("./routes/home.js")
 const port = process.env.PORT || 3000;
 
 
+app.set('views', './views')
+app.set('view engine', 'pug')
+
+
+
+//Routes
 app.use(home)
 
 
--- routes/home.js
+++ routes/home.js
@@ -3,11 +3,11 @@ const router = express.Router();
 
 
 router.get('/', function (req, res) {
-    res.send("Página principal");
+    res.render('index');
 });
 
 router.get('/otra', function (req, res) {
-    res.send("Esta esla otra página");
+
 });
 
 module.exports = router;
