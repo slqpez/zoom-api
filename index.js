89212c463db2ce06af0811a33fffc50d201b75b4
slqpez <santiago.lopez16@udea.edu.co>
2020-09-05 19:01:46 -0500

    Static content

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 const home = require("./routes/home.js")
+const path = require("path")
 
 const port = process.env.PORT || 3000;
 
 
 app.set('views', './views')
 app.set('view engine', 'pug')
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 
 
---
+++ public/js/app.js
@@ -0,0 +1,1 @@
+console.log("Este es el frontend pa'")
